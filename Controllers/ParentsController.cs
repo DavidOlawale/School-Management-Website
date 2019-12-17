4df8d8b20302e254b9dd7d3e1e057996fc262260
davidolawale <olawaledavid11@gmail.com>
2019-12-17 20:51:27 +0100

    Add Parent controller
    parent controller contain actions for Viewing all, viewing details of, editing and deleting Parents

---
+++ Controllers/ParentsController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using School.Data;
+using School.Models;
+
+namespace School.Controllers
+{
+    public class ParentsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private bool ParentExists(Guid id) => _context.Parents.Any(e => e.Id == id);
+        public ParentsController(ApplicationDbContext context) => _context = context;
+
+        public IActionResult Index()
+        {
+            ViewData["isToast"] = false;
+            return View(_context.Parents);
+        }
+
+        public async Task<IActionResult> Details(Guid id)
+        {
+            var parent = await _context.Parents.FindAsync(id);
+            if (parent == null)
+                return NotFound();
+
+            return View(parent);
+        }
+
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Parent parent)
+        {
+            if (!ModelState.IsValid)
+                return View(parent);
+            parent.Id = Guid.NewGuid();
+            _context.Add(parent);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var parent = await _context.Parents.FindAsync(id);
+            if (parent == null)
+                return NotFound();
+            return View(parent);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id,  Parent parent)
+        {
+            if (id != parent.Id)
+                return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(parent);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ParentExists(parent.Id))
+                        return NotFound();
+                    else
+                        throw;
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(parent);
+        }
+
+        // GET: Parents/Delete/5
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var parent = await _context.Parents
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (parent == null)
+                return NotFound();
+            return View(parent);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var parent = await _context.Parents.FindAsync(id);
+            _context.Parents.Remove(parent);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
--- Models/AppModel.cs
+++ Models/AppModel.cs
@@ -221,7 +221,7 @@ namespace School.Models
     {
         public int Id { get; set; }
         public Guid SenderId { get; set; }
-        public Guid RecieverId { get; set; }
+        public Guid ReceiverId { get; set; }
         public string Content { get; set; }
         public bool Received { get; set; }
     }
