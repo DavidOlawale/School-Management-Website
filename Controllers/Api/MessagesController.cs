a365e32bdf682b3bd3a899e824db94d62a426242
davidolawale <olawaledavid11@gmail.com>
2019-12-30 13:43:47 +0100

    Removing ignored files

--- Controllers/Api/MessagesController.cs
+++ Controllers/Api/MessagesController.cs
@@ -13,32 +13,32 @@ namespace School.Controllers.Api
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class TeacherMessagesController : ControllerBase
+    public class MessagesController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
         private readonly UserManager<ApplicationUser> _manager;
 
-        public TeacherMessagesController(ApplicationDbContext context, UserManager<ApplicationUser> manager)
+        public MessagesController(ApplicationDbContext context, UserManager<ApplicationUser> manager)
         {
             _context = context;
             _manager = manager;
         }
 
-        [HttpGet]
-        public async Task<IEnumerable<Message>> GetRecievedMessagesAsync()
+        [HttpGet("Received")]
+        public async Task<IEnumerable<Message>> Received()
         {
             Guid UserId = (await _manager.GetUserAsync(User)).Id;
-            return _context.Messages.Where(m => m.ReceiverId == UserId);
+            return _context.Messages.Where(m => m.ReceiverId == UserId).Include(m => m.Sender);
         }
 
-        [HttpGet]
-        public async Task<IEnumerable<Message>> GetSentMessagesAsync()
+        [HttpGet("Sent")]
+        public async Task<IEnumerable<Message>> Sent()
         {
             Guid UserId = (await _manager.GetUserAsync(User)).Id;
             return _context.Messages.Where(m => m.SenderId == UserId);
         }
 
-        [HttpPost]
+        [HttpPost("postmessage")]
         public async Task<ActionResult<Message>> PostMessage(Message message)
         {
             _context.Messages.Add(message);
@@ -47,11 +47,5 @@ namespace School.Controllers.Api
             return CreatedAtAction("GetMessage", new { id = message.Id }, message);
         }
 
-        public async Task<ActionResult<Message>> PostParentMessageAsync(Message message)
-        {
-            _context.Messages.Add(message);
-            await _context.SaveChangesAsync();
-            return CreatedAtAction(nameof(GetSentMessagesAsync), new { }, message);
-        }
     }
 }
--- Models/AppModel.cs
+++ Models/AppModel.cs
@@ -223,6 +223,7 @@ namespace School.Models
     {
         public int Id { get; set; }
         public Guid SenderId { get; set; }
+        public ApplicationUser Sender { get; set; }
         public Guid ReceiverId { get; set; }
         public string Content { get; set; }
         public bool Received { get; set; }
