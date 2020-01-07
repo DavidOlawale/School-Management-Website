fd8684ca916ead2fa02b68d9eea1048e972bb57f
davidolawale <olawaledavid11@gmail.com>
2020-01-07 23:21:45 +0100

    Change static getters of RoleName class to constant fields
    getters of RoleName class such as Admin and Teacher are changed to constant fields so they can be use as Authorize attribute

--- Models/AppModel.cs
+++ Models/AppModel.cs
@@ -12,10 +12,10 @@ namespace School.Models
 {
     public static class RoleNames
     {
-        public  static string Admin => "Admin";
-        public static string Student => "Student";
-        public static string Teacher => "Teacher";
-        public static string Parent => "Parent";
+        public  const string Admin = "Admin";
+        public const string Student = "Student";
+        public const string Teacher = "Teacher";
+        public const string Parent = "Parent";
 
     }
 
